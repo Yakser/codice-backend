348eef4c319cef5d88b5c1f0ec6d356a841a6575
Yakser <iaksanov2044@mail.ru>
2023-08-19 20:25:21 +0300

    Add language field

--- app/pastes/router.py
+++ app/pastes/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from sqlmodel import Session, select
+from starlette import status
 
 from app.db import engine
 from app.pastes.models import Paste
@@ -17,12 +18,22 @@ async def pastes_list() -> list[Paste]:
         return session.exec(select(Paste)).all()
 
 
+@router.get("/{slug}", description="Returns list of all pastes")
+async def paste_detail(slug: str) -> Paste:
+    with Session(engine) as session:
+        paste = session.exec(select(Paste).where(Paste.slug == slug)).first()
+        if paste:
+            return paste
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
 @router.post("/")
 async def add_paste(paste: Paste) -> Paste:
     with Session(engine) as session:
         if session.exec(select(Paste).where(Paste.slug == paste.slug)).all():
             raise HTTPException(
-                status_code=422, detail="Paste with given slug already exists!"
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail="Paste with given slug already exists!",
             )
 
         session.add(paste)
